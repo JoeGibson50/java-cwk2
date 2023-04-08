c8ae20d17b1e4d17f0bf5e8b005a346f2acfee06
Joe Gibson <joegibson@Davids-MacBook-Pro.local>
2023-04-08 13:03:57 +0100

    Fixed cardValue test

--- src/main/java/BaccaratCard.java
+++ src/main/java/BaccaratCard.java
@@ -4,4 +4,17 @@ public class BaccaratCard extends Card {
     public BaccaratCard(Card.Rank rank, Card.Suit suit) {
         super(rank, suit);
     }
+    
+    /**
+     * Computes the value of this card for BaccaratCard.
+     *
+     * <p>Utilise value from superclass and return 0 if equal to 10.</p>
+     *
+     * @return Card value
+     */
+    @Override
+    public int value() {
+         int cardValue = super.value();
+         return cardValue == 10 ? 0 : cardValue;
+    }
 }
